9298f13c34f18dbdc6af2ed7316da89f5d9f3367
Alex Leith <alexgleith@gmail.com>
2024-03-19 13:24:30 +1100

    Update availability audit notebooks

---
+++ availability_audit/utils.py
@@ -0,0 +1,17 @@
+import json
+from shapely.geometry import shape
+
+# A bounding box over all Australia
+bbox = [112.0, -44.0, 155.0, -11.0]
+
+
+def get_australia_coords():
+    with open("australia.geojson") as f:
+        australia = json.load(f)
+        return australia['features'][0]['geometry']['coordinates'][0]
+
+
+def get_australia_geometry():
+    with open("australia.geojson") as f:
+        australia = json.load(f)
+        return shape(australia['features'][0]['geometry'])
--- utils.py
+++
@@ -1,2 +0,0 @@
-# A bounding box over all Australia
-bbox = [112.0, -44.0, 155.0, -10.0]
