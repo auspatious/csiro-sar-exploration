d6fe2229a8b4a162b986ca7e8f0fdc756453693d
Alex Leith <alexgleith@gmail.com>
2024-04-26 15:00:16 +1000

    Fix formatting

--- availability_audit/utils.py
+++ availability_audit/utils.py
@@ -8,10 +8,10 @@ bbox = [112.0, -44.0, 155.0, -11.0]
 def get_australia_coords():
     with open("australia.geojson") as f:
         australia = json.load(f)
-        return australia['features'][0]['geometry']['coordinates'][0]
+        return australia["features"][0]["geometry"]["coordinates"][0]
 
 
 def get_australia_geometry():
     with open("australia.geojson") as f:
         australia = json.load(f)
-        return shape(australia['features'][0]['geometry'])
+        return shape(australia["features"][0]["geometry"])
