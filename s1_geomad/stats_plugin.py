a75149eadb4c5761610b4401bd8a0ffecfe33878
Alex Leith <alexgleith@gmail.com>
2024-04-26 13:58:51 +1000

    Set up as a package

--- s1_geomad/stats_plugin.py
+++ s1_geomad/stats_plugin.py
@@ -5,10 +5,17 @@ Sentinel-1 mosaic
 from typing import Tuple
 
 import xarray as xr
-
 from odc.stats.plugins._registry import StatsPluginInterface, register
 
-MEASUREMENTS = ["mean_vv", "mean_vh", "median_vv", "median_vh", "std_vv", "std_vh", "count"]
+MEASUREMENTS = [
+    "mean_vv",
+    "mean_vh",
+    "median_vv",
+    "median_vh",
+    "std_vv",
+    "std_vh",
+    "count",
+]
 
 
 class S1Mosaic(StatsPluginInterface):
@@ -17,43 +24,28 @@ class S1Mosaic(StatsPluginInterface):
     VERSION = "0.0.0"
     PRODUCT_FAMILY = "general"
 
-    def __init__(
-        self,
-        **kwargs
-    ):
+    def __init__(self, **kwargs):
         super().__init__(input_bands=["vv", "vh"], **kwargs)
 
     @property
     def measurements(self) -> Tuple[str, ...]:
-        measurements = ["mean_vv", "mean_vh"]
-
-        return tuple(measurements)
+        return tuple(MEASUREMENTS)
 
     def fuser(self, xx: xr.Dataset) -> xr.Dataset:
-        """
-        """
-        # Nothing to do here...
-
-        return xx
-
-    def native_transform(self, xx: xr.Dataset) -> xr.Dataset:
-        """
-        """
-        # Nothing to do here either
-        return xx
+        """ """
+        # Make nodata nan
+        return xx.where(xx != xx.vv.nodata)
 
     def reduce(self, xx: xr.Dataset) -> xr.Dataset:
-        """
-        """
-
+        """ """
         arrays = []
         for band in ["vv", "vh"]:
-            arrays.append(xx[band].median("time").rename(f"median_{band}"))
-            arrays.append(xx[band].mean("time").rename(f"mean_{band}"))
-            arrays.append(xx[band].std("time").rename(f"std_{band}"))
+            arrays.append(xx[band].median(axis=0).rename(f"median_{band}"))
+            arrays.append(xx[band].mean(axis=0).rename(f"mean_{band}"))
+            arrays.append(xx[band].std(axis=0).rename(f"std_{band}"))
 
         # Add count
-        arrays.append(xx["vv"].count("time").rename("count").astype("int16"))
+        arrays.append(xx["vv"].count(axis=0).rename("count").astype("int16"))
 
         # Merge the arrays together into a Dataset with the names we want
         mosaic = xr.merge(arrays, compat="override")
